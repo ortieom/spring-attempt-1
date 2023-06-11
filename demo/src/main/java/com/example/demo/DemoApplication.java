6eed426be47247e39f4499e6e5d0969565ff52e1
LeonidM <wildhs.botm@gmail.com>
2023-06-11 15:37:41 +0300

    REVIEW

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,12 @@ import org.springframework.context.ApplicationContext;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// REVIEW: Сделай отдельный контроллер, который на вход принимает строку формата "Число1 + Число2"
+        // и который возвращает результат (для простоты сделай эти числа как double). Не подключай его к вебу, просто возьми
+        // этот бин из контекста и ручками вызови метод. Для такого случая лучше убрать Multiplier и Adder, а сделать
+		// отдельный интерфейс Calculator с методами сложения, умножениями и другими операторами, а уже далее сделать
+		// его реализации типа DoubleCalculator и IntegerCalculator, которые будут компонентами
+
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 		System.out.println(context.getBean("multiplier", Multiplier.class).eval(1, 2));
 		System.out.println(context.getBean("adder", Adder.class).eval(1, 2));
--- demo/src/main/java/com/example/demo/Operation.java
+++ demo/src/main/java/com/example/demo/Operation.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+// REVIEW: Я так полагаю, что это лишний класс, который ты забыл убрать, если это так, то убери его
 public interface Operation {
     int eval();
 }
